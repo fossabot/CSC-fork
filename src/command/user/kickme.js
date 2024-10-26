d3400a35f93410dc6b86bff5f84c82b3d66a2ae5
Rukmani Devi <168568132@qq.com>
2024-10-26 22:36:28 +0800

    无.

--- src/command/user/kickme.js
+++ src/command/user/kickme.js
@@ -2,6 +2,7 @@
 export async function run(hazel, core, hold, socket, data) {
   // 先检查昵称
   if (!core.verifyNickname(data.nick) && data.nick !== '') {
+    // 如果昵称格式不正确
     core.replyWarn('NICKNAME_INVALID', '昵称应当仅由汉字、字母、数字和不超过 3 个的特殊字符（_-+.:;）组成，而且不能太长。', socket);
     return;
   }
@@ -14,11 +15,13 @@ export async function run(hazel, core, hold, socket, data) {
       core.replyWarn('USER_NOT_FOUND', '在这个聊天室找不到您指定的用户。', socket);
       return;
     }
-
+    // 过滤掉自己
     targetSockets = targetSockets.filter(targetSocket => targetSocket.connectionID !== socket.connectionID);
   
+    // 告知用户共有多少个目标
     core.replyInfo('KICKED_INFO', '共有 ' + targetSockets.length + ' 个目标。', socket, { nick: socket.nick });
   
+    // 逐个踢出
     targetSockets.forEach(targetSocket => {
       if (targetSocket.remoteAddress === socket.remoteAddress) {
         core.replyInfo('KICKED_BY_SELF', '您已经被您自己断开连接，如果不是您自己执行的操作，请重新加入并通知管理员。', targetSocket);
@@ -26,6 +29,7 @@ export async function run(hazel, core, hold, socket, data) {
         targetSocket.close();
       }
       else {
+        // 如果目标用户不是自己卡住的 socket
         core.replyWarn('KICKME_FAILED', '您指定的用户可能不是您自己。', socket);
       }
     });
@@ -41,11 +45,14 @@ export async function run(hazel, core, hold, socket, data) {
       core.replyWarn('USER_NOT_FOUND', '在这个聊天室找不到您指定的用户。', socket);
       return;
     }
+    // 按理来说，targetSocket 应该只有一个元素
     [targetSocket] = targetSocket;
+    // 如果目标用户是用户自己
     if (targetSocket.connectionID === socket.connectionID) {
       core.replyWarn('KICKME_FAILED', '您指定的用户是您自己。', socket);
       return;
     }
+    // 如果目标用户是用户自己卡住的 socket
     if ((() => {
       if (targetSocket.remoteAddress === socket.remoteAddress) { return true; }
       return false;
